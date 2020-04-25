6ef9e2111b37f18db0066ca9b2181b03bbcba462
LucianoCarbonera <luciano.c10@aluno.ifsc.edu.br>
2020-04-24 22:12:30 -0300

    Relação N:N

--- laravel/app/Venda.php
+++ laravel/app/Venda.php
@@ -8,4 +8,17 @@ class Venda extends Model
 {
     protected $table = 'vendas';
     protected $primaryKey ='id';
+
+    function cliente()
+    {
+        return $this->belongsTo('App\Cliente', 'id_cliente', 'id');
+    }
+
+    function produtos()
+    {
+        return $this->belongsToMany('App\Produto', 'produtos_vendas',
+            'id_venda', 'id_produto')
+            ->withPivot(['id','quantidade','subtotal'])
+            ->withTimestamps();
+    }
 }
