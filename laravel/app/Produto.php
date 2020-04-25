d5f4e489c045d78efe05e1746caa463ebf93219f
Welinton Haas <welinton.haas@gmail.com>
2020-04-24 22:45:48 -0300

    ajuste Produto

--- laravel/app/Produto.php
+++ laravel/app/Produto.php
@@ -4,14 +4,15 @@ namespace App;
 
 use Illuminate\Database\Eloquent\Model;
 
-class Produto extends Model{
-     protected $table =  'produtos';
-     protected $primarykey = 'id';
+class Produto extends Model
+{
+    protected $table = 'produtos';
+    protected $primarykey = 'id';
 
-function cliente()  {
+    function cliente()
+    {
         return $this->belongsTo('App\Cliente', 'id_cliente', 'id');
     }
+}
 
-function cliente(){
-        return $this->belongsTo('App\Cliente', 'id_cliente', 'id');
- }
+
